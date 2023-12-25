05f079ee09e6567ad79d760aef01f1047454c861
khai123-2 <tranquangkhai0910@gmail.com>
2023-12-25 14:03:04 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '46cd42532aed81f525becc0a56d0520b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '923ab835-8dd7-49a9-ba95-2cae26511d2d'
+};
